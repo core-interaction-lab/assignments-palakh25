1e178e376c48f8f76c624b877f1e967b5f0226ca
palakh25 <khetp523@newschool.edu>
2022-03-08 19:15:02 -0500

    d

---
+++ exercises/exercise-5/js/main.js
@@ -0,0 +1,3 @@
+alert('This is exercise 5!');
+
+const myButton = document.getElementById
---
+++ projects/project-2/script.js
@@ -0,0 +1,214 @@
+function myFunction() {
+    // alert("I am an alert box!");
+    console.log("card1 was clicked ");
+    
+    var paragraph= document.getElementById("paragraph1");
+    var card=document.getElementById("card1");
+
+    if(paragraph.style.display=="none" ) 
+    {paragraph.style.display="block";
+  
+    card.style.zIndex=19;} 
+    else{
+     paragraph.style.display="none";
+     card.style.zIndex=0;
+
+    }
+}
+
+function functioncard2() {
+    console.log("card2 was clicked");
+
+    var paragraph= document.getElementById("paragraph2");
+    var card=document.getElementById("card2");
+
+    if(paragraph.style.display=="none")
+    {paragraph.style.display="block";
+
+        card.style.zIndex=18; }
+else{
+    paragraph.style.display="none";
+    card.style.zIndex=0;
+
+}
+}
+function functioncard3() {
+    console.log("card3 was clicked");
+
+    var paragraph= document.getElementById("paragraph3");
+    var card=document.getElementById("card3");
+
+    if(paragraph.style.display=="none")
+    {paragraph.style.display="block";
+
+card.style.zIndex=17; }
+else{
+    paragraph.style.display="none";
+    card.style.zIndex=0;
+
+}
+}
+
+function functioncard4() {
+    console.log("card4 was clicked");
+
+    var paragraph= document.getElementById("paragraph4");
+    var card=document.getElementById("card4");
+
+    if(paragraph.style.display=="none")
+    {paragraph.style.display="block";
+
+card.style.zIndex=16; }
+else{
+    paragraph.style.display="none";
+    card.style.zIndex=0;
+
+}
+}
+function functioncard5() {
+    console.log("card5 was clicked");
+
+    var paragraph= document.getElementById("paragraph5");
+    var card=document.getElementById("card5");
+
+    if(paragraph.style.display=="none")
+    {paragraph.style.display="block";
+
+card.style.zIndex=15; }
+else{
+    paragraph.style.display="none";
+    card.style.zIndex=0;
+
+}
+}
+function functioncard6() {
+    console.log("card6 was clicked");
+
+    var paragraph= document.getElementById("paragraph6");
+    var card=document.getElementById("card6");
+
+    if(paragraph.style.display=="none")
+    {paragraph.style.display="block";
+
+card.style.zIndex=14; }
+else{
+    paragraph.style.display="none";
+    card.style.zIndex=0;
+
+}
+}
+function functioncard7() {
+    console.log("card7 was clicked");
+
+    var paragraph= document.getElementById("paragraph7");
+    var card=document.getElementById("card7");
+
+    if(paragraph.style.display=="none")
+    {paragraph.style.display="block";
+
+card.style.zIndex=13; }
+else{
+    paragraph.style.display="none";
+    card.style.zIndex=0;
+
+}
+}
+
+function functioncard8() {
+    console.log("card8 was clicked");
+
+    var paragraph= document.getElementById("paragraph8");
+    var card=document.getElementById("card8");
+
+    if(paragraph.style.display=="none")
+    {paragraph.style.display="block";
+
+card.style.zIndex=12; }
+else{
+    paragraph.style.display="none";
+    card.style.zIndex=0;
+
+}
+}
+function functioncard9() {
+    console.log("card9 was clicked");
+
+    var paragraph= document.getElementById("paragraph9");
+    var card=document.getElementById("card9");
+
+    if(paragraph.style.display=="none")
+    {paragraph.style.display="block";
+
+card.style.zIndex=11; }
+else{
+    paragraph.style.display="none";
+    card.style.zIndex=0;
+
+}
+}
+
+function functioncard10() {
+    console.log("card10 was clicked");
+
+    var paragraph= document.getElementById("paragraph10");
+    var card=document.getElementById("card10");
+
+    if(paragraph.style.display=="none")
+    {paragraph.style.display="block";
+
+card.style.zIndex=10; }
+else{
+    paragraph.style.display="none";
+    card.style.zIndex=0;
+
+}
+}
+
+function functioncard11() {
+    console.log("card11 was clicked");
+
+    var paragraph= document.getElementById("paragraph11");
+    var card=document.getElementById("card11");
+
+    if(paragraph.style.display=="none")
+    {paragraph.style.display="block";
+
+card.style.zIndex=9; }
+else{
+    paragraph.style.display="none";
+    card.style.zIndex=0;
+
+}
+}
+function functioncard12() {
+    console.log("card12 was clicked");
+
+    var paragraph= document.getElementById("paragraph12");
+    var card=document.getElementById("card12");
+
+    if(paragraph.style.display=="none")
+    {paragraph.style.display="block";
+
+card.style.zIndex=8; }
+else{
+    paragraph.style.display="none";
+    card.style.zIndex=0;
+
+}
+}
+function functioncard13() {
+    console.log("card13 was clicked");
+
+    var paragraph= document.getElementById("paragraph13");
+    var card=document.getElementById("card13");
+
+    if(paragraph.style.display=="none")
+    {paragraph.style.display="block";
+
+card.style.zIndex=7; }
+else{
+    paragraph.style.display="none";
+    card.style.zIndex=0;
+
+}
+}
