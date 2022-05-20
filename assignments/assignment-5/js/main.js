65897a863bad83c284674dee42808b5d0e4d56b3
palakh25 <khetp523@newschool.edu>
2022-05-20 01:00:14 -0400

    d

--- assignments/assignment-5/js/main.js
+++ assignments/assignment-5/js/main.js
@@ -20,6 +20,6 @@ var name = document.getElementById("name").value;
    
     storyDiv.innerHTML =
   
-  "One day" +'&nbsp' + name  +'&nbsp' + "and his friend were lying on a"  +'&nbsp' + place  +'&nbsp' +"and enjoying the"  +'&nbsp' +noun   +'&nbsp' +". It was a"  +'&nbsp' +adjective +'&nbsp' +"day, and they were smiling to themselves."  + "Seeing his friend,"  +'&nbsp' +name  +'&nbsp' +"inquired as to what was causing him to smile. His friend replied saying he was thinking about the day when he will be truly happy"+
+  "One day" +'&nbsp' + name   +'&nbsp' + "and his friend were lying on a"  +'&nbsp' + place  +'&nbsp' +"and enjoying the"  +'&nbsp' +noun   +'&nbsp' +". It was a"  +'&nbsp' +adjective +'&nbsp' +"day, and they were smiling to themselves."  + "Seeing his friend,"  +'&nbsp' +name  +'&nbsp' +"inquired as to what was causing him to smile. His friend replied saying he was thinking about the day when he will be truly happy"+
 "“When is that?”"  +'&nbsp' +name  +'&nbsp' + "asked. His friend went on to explain that he will truly feel happy when he has"  +'&nbsp' +noun1  +'&nbsp' + ","  +'&nbsp' +noun2 +"," +'&nbsp' + noun3 +"," +'&nbsp'  + "a pretty"  +'&nbsp'  +noun4  +'&nbsp' + "and four"  +'&nbsp' +noun5  +'&nbsp' + "who would get educated and earn a lot of" +'&nbsp' + noun6 +"."+
   `Interrupting this monologue,&nbsp${name}&nbspasked, “After all this, what will you do?” To which his friend replies “After all this, I can &nbsp${verb}. Hearing this,&nbsp${name}&nbspgives out a mighty laugh!` ;}
